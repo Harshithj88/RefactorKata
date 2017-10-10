adb67ff142405f0998f342f8e063a633e752f402
Harshith Julapelli <harsh.julapelli@gmail.com>
2017-10-10 14:17:54 -0500

    Removed Comments

--- RefactorKata/RefactorKata/Program.cs
+++ RefactorKata/RefactorKata/Program.cs
@@ -4,22 +4,15 @@ using System.Data.SqlClient;
 
 namespace RefactorKata
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            //This is intentionally bad : (  Let's Refactor!
             var conn = new System.Data.SqlClient.SqlConnection("Server=.;Database=myDataBase;User Id=myUsername;Password = myPassword;");
-
             var cmd = conn.CreateCommand();
             cmd.CommandText = "select * from Products";
-            /*
-             * cmd.CommandText = "Select * from Invoices";
-             */
             var reader = cmd.ExecuteReader();
             var products = new List<Product>();
-
-            //TODO: Replace with Dapper
             while (reader.Read())
             {
                 var prod = new Product { Name = reader["Name"].ToString() };
@@ -35,8 +28,6 @@ namespace RefactorKata
     }
     public class Product
     {
-
-
         public string Name
         {
             get;
