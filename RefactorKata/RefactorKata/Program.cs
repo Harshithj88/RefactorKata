8bf0acbe9628556b82d8f18cfbb6ead6982b0e19
Harshith Julapelli <harsh.julapelli@gmail.com>
2017-10-10 13:50:51 -0500

    Implicity declared connection

--- RefactorKata/RefactorKata/Program.cs
+++ RefactorKata/RefactorKata/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 
 namespace RefactorKata
 {
@@ -8,14 +9,14 @@ namespace RefactorKata
         static void Main(string[] args)
         {
             //This is intentionally bad : (  Let's Refactor!
-            System.Data.SqlClient.SqlConnection Conn = new System.Data.SqlClient.SqlConnection("Server=.;Database=myDataBase;User Id=myUsername;Password = myPassword;");
+            var conn = new System.Data.SqlClient.SqlConnection("Server=.;Database=myDataBase;User Id=myUsername;Password = myPassword;");
 
-            System.Data.SqlClient.SqlCommand cmd = Conn.CreateCommand();
+            var cmd = conn.CreateCommand();
             cmd.CommandText = "select * from Products";
             /*
              * cmd.CommandText = "Select * from Invoices";
              */
-            System.Data.SqlClient.SqlDataReader reader = cmd.ExecuteReader();
+            var reader = cmd.ExecuteReader();
             List<Product> products = new List<Product>();
 
             //TODO: Replace with Dapper
@@ -25,7 +26,7 @@ namespace RefactorKata
                 prod.name = reader["Name"].ToString();
                 products.Add(prod);
             }
-            Conn.Dispose();
+            conn.Dispose();
             Console.WriteLine("Products Loaded!");
             for (int i =0; i< products.Count; i++)
             {
